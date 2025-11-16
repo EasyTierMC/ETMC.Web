ebe6251b346945784527e1dd9a728c49b16241d7
Tianpao <nxtianpao@163.com>
2025-11-16 14:31:01 +0800

    feat：主页地图

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,10 @@
   <section style="padding:2rem; text-align:center;">
     <h2>欢迎来到 EasyTierMC Uptime</h2>
     <p>使用上方导航查看节点监控或提交新的节点。</p>
+    <Map />
   </section>
 </template>
+
+<script setup lang="ts">
+import Map from '@/components/dashboard/Map.vue';
+</script>
